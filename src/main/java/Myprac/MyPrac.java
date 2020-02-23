60ad9ae3c908d255021a120f75c9d6eb8d573ce4
Luzuko Tshaka <luzukotshaka2@gmail.com>
2020-02-23 22:36:31 +0200

    Initial commit: this was just a practise ignore it

---
+++ src/main/java/Myprac/MyPrac.java
@@ -0,0 +1,12 @@
+package Myprac;
+public class MyPrac {
+
+   private static int x = 34, y = 4;
+
+    public static void main(String[] args) {
+
+        int sum = x + y;
+
+        System.out.println(sum);
+    }
+}
